6a1d4b8151e0ab07801c6351bb928b4b60f0ac26
mire <mhriengusse35@gamil.com>
2023-06-19 09:39:33 +0300

    my first commit

---
+++ Mirem.js
@@ -0,0 +1,28 @@
+function changeText (){
+    var texts
+}
+
+var p1 = document.getElementById('p1')
+var p2 = document.getElementById('p2')
+p1.textContent =text[0]. testimonial
+p2.textContent =text[1]. name
+if(index<text.length-1)
+    index = index+1
+else(index=0>)    
+
+
+setTimeout(changeText(text),100m)
+{ 
+    testimonial1:"hehehjehe";
+    name1:"john";
+}
+{
+    testimonial2:"hehehjehe";
+    name2:"tomy";
+}
+var inputDone= true
+while (inputDone)
+var testimonial= prompt('textmonial')
+var name= prompt('name?')
+text.push(testimonial:testimonial' name: ssname)
+inputDone = prompt("put 1 if done 0 is not?")
---
+++ example.js
@@ -0,0 +1,4 @@
+function changeImage(mir) {
+    var displayImg=document.getElementById('mira')
+    displayImg.src=mir.src   
+}
